6bea226ea282ca43ec7417b6806393321f2da20d
DeveNinja <abbays10@gmail.com>
2018-12-19 22:37:53 +0800

    moving along with the course

--- src/exercise2/SeasonDisplay.js
+++ src/exercise2/SeasonDisplay.js
@@ -1,47 +1,22 @@
 import React, { Component } from 'react'
 
-export class SeasonDisplay extends Component {
+class SeasonDisplay extends Component {
   constructor(props){
     super(props)
     this.state = {
-      Date: new Date(),
-      clientLocation: window.navigator.geolocation
+      latt: null,
+      long: null
     }
-    
   }
-  componentDidMount() {  
-    // Be careful, the Date() constructor uses 0-indexed month so May is
-    // represented with 4 (and not 5)
-    this.setState({
-      // month: this.state.Date.getMonth(),
-      month: this.state.Date.getMonth(),
-      today: this.state.Date.getDay(),
-      time: this.state.Date.getTime(),
-      clientLocalTime: this.state.Date.getTimezoneOffset() / 60,
-
-      // Get the client current position after client's approval
-      clientLocation: this.state.clientLocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-      )
-    })    
-  }
-
   render() {
-    const {
-      Date,
-      month,
-      today,
-      time,
-      clientLocation
-    } = this.state
-    return (
+    // Get the client current position after client's approval
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    )
+    return (      
       <div>
-        {today}
-        <br/>
-        {month}
-        <br/>
-        {time}
+        <h4>Lattitude: {this.state.latt}</h4>
       </div>
     )
   }
