c2d1079e52dd75bcdc8248e7378bcf2d2e841e4e
DeveNinja <abbays10@gmail.com>
2018-12-19 20:07:56 +0800

    date and time

--- src/exercise2/SeasonDisplay.js
+++ src/exercise2/SeasonDisplay.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
 
 export class SeasonDisplay extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      Date: new Date(),
+      clientLocation: window.navigator.geolocation
+    }
+    
+  }
+  componentDidMount() {  
+    // Be careful, the Date() constructor uses 0-indexed month so May is
+    // represented with 4 (and not 5)
+    this.setState({
+      // month: this.state.Date.getMonth(),
+      month: this.state.Date.getMonth(),
+      today: this.state.Date.getDay(),
+      time: this.state.Date.getTime(),
+      clientLocalTime: this.state.Date.getTimezoneOffset() / 60,
+
+      // Get the client current position after client's approval
+      clientLocation: this.state.clientLocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err)
+      )
+    })    
+  }
+
   render() {
+    const {
+      Date,
+      month,
+      today,
+      time,
+      clientLocation
+    } = this.state
     return (
       <div>
-        
+        {today}
+        <br/>
+        {month}
+        <br/>
+        {time}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SeasonDisplay from './exercise2/SeasonDisplay';
+import SeasonDisplay from './exercise2/SeasonDisplay'
 // import Components from './exercise1/Components'
 
 const Root = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  )
   return (
     <SeasonDisplay />
   )
